cfdfb334682fc107983e40d5be0b66deecdc1325
Corbett Winningham <Corbett Winningham>
2021-08-19 16:55:50 -0700

    Oh snap, I did a bunch of stuff on this one: got the js running, websockets, installed Redis, etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
